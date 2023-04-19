7d8b61c04ee793a5ece7ef43ff75eded3a733464
erlangram <erlanggaramadhan424@gmail.com>
2023-04-19 21:54:24 +0700

    tugaswp2

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Erlangga</li>
+ <li>Nama Belakang :Ramadhan</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Kepu timur nomer12</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>futsal</li>
+ <li>ngojek</li>
+ </ul>
+</section>
